8532d3ee5c79fa38ab63a88b94154a2ddfccb31e
uneyss <73793862+uneyss@users.noreply.github.com>
2021-04-25 01:27:44 +0300

    Update
    Delete function added

--- toDo.js
+++ toDo.js
@@ -10,10 +10,6 @@ function newElement()
     let liNode = document.createElement('li')
     let textnode = document.createTextNode(inputValue)
     
-
-
-
-
      if(inputValue === "" || $.trim( $('#task').val() ) == '' ){
          $(".error").toast("show");
         console.log("işlem geçersiz")
@@ -21,9 +17,50 @@ function newElement()
         console.log("işlem geçerli")
         $(".success").toast("show");
         liNode.innerHTML = 
-            `<span class="close">x</span>`
+            `<span class="close" onclick="myFunction()">x</span>`
         
         liNode.appendChild(textnode) 
         listNode.appendChild(liNode);
+
     } 
 }
+
+/* function deleteElement() 
+{
+    let lis = document.querySelector('#list li');
+    for(let i=0; li = lis[i]; i++) {
+        li.parentNode.removeChild(li);
+    }
+} */
+
+function myFunction() {
+    var list = document.getElementById("list");
+    list.removeChild(list.childNodes[0]);
+  }
+/* function myFunction() {
+    var ulElem = document.getElementById('list');
+
+    ulElem.removeChild(ulElem.childNodes[getSelection])
+  } */
+
+
+/*   var inputText = document.getElementById("txt"),
+  items = document.querySelectorAll("#list li"),
+  tab = [], index;
+  function refreshArray()
+  {
+      // clear array
+      tab.length = 0;
+      items = document.querySelectorAll("#list li");
+      // fill array
+      for(var i = 0; i < items.length; i++){
+       tab.push(items[i].innerHTML);
+     }
+  }
+
+  function deleteLI(){
+    var myList = document.getElementById('myList');
+    myList.innerHTML = '';
+}
+ */
+  
